a3b500f68b23373368dd8ee4323e8e706a37be8d
vinit <vinitkkulkarni@gmail.com>
2023-04-11 15:04:12 +0530

    Added:if condition used

--- EmpWageTest.java
+++ EmpWageTest.java
@@ -4,8 +4,13 @@ public class EmpWageTest {
     public static void main(String[] args) {
         System.out.println("welcome to emp wage program");
 
+        int isPresent = (int)(Math.random()*10)%2;
+        System.out.println(isPresent);
 
-        int ispresent = (int)(Math.random()*10)%3;
-        System.out.println(ispresent);
+        if(isPresent == 1){
+            System.out.println("emp is present");
+        }else{
+            System.out.println("emp is absent");
+        }
     }
 }
