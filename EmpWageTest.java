95b0118c2b1db6a82178b9e0d9fd5bc7d9e5a3b9
vinit <vinitkkulkarni@gmail.com>
2023-04-11 14:37:38 +0530

    Added:generated random number

--- EmpWageTest.java
+++ EmpWageTest.java
@@ -3,5 +3,9 @@ package com.bridgelabz.githubtest;
 public class EmpWageTest {
     public static void main(String[] args) {
         System.out.println("welcome to emp wage program");
+
+
+        int ispresent = (int)(Math.random()*10)%3;
+        System.out.println(ispresent);
     }
 }
